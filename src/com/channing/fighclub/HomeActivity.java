324ac3405b65309804b031eabde0ca0d2643cf42
Peter Lee <nadapeter@gmail.com>
2013-02-04 17:16:26 -0800

    Async loading for homescreen

--- src/com/channing/fighclub/HomeActivity.java
+++ src/com/channing/fighclub/HomeActivity.java
@@ -6,7 +6,7 @@ import org.json.JSONException;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -18,14 +18,18 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import com.faceapp.SampleFaceAppActivity;
 import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
 
 public class HomeActivity extends Activity {
 	
 	private Context context;
 	private static final String TAG = "HomeActivity";
-	
+
+
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,9 +38,12 @@ public class HomeActivity extends Activity {
         context = getApplicationContext();
         setContentView(R.layout.activity_main);
         setUpClickListensers();
-        loadContents();
-        
-        
+
+        //loadContents();
+        LoadFeaturedContentsTask lfc = new LoadFeaturedContentsTask(context);
+        lfc.execute();
+        LoadCategoryContentsTask lcc = new LoadCategoryContentsTask(context);
+        lcc.execute();
     }
 
     @Override
@@ -152,8 +159,12 @@ public class HomeActivity extends Activity {
         	for (int i=0; i < featuredProducts.length(); i++) {
         		String url = JsonUtil.handleJsonObject
         				(featuredProducts.getString(i), "icon");
-                String title = JsonUtil.handleJsonObject(featuredProducts.getString(i), "name");
-            	String id = JsonUtil.handleJsonObject(featuredProducts.getString(i), "id");
+                String title = 
+                		JsonUtil.handleJsonObject(
+                				featuredProducts.getString(i), "name");
+            	String id = 
+            			JsonUtil.handleJsonObject(
+            					featuredProducts.getString(i), "id");
         		ImageView iv = new ImageView(this);
             	iv.setImageResource(R.drawable.loading);
             	int minDimen = Util.dpToPx(
@@ -273,6 +284,199 @@ public class HomeActivity extends Activity {
             
         } catch (JSONException e) {
         	Log.v(TAG, "Json parsing error: " + e);
+        }
+    }
+    
+    
+    private class LoadFeaturedContentsTask extends AsyncTask<Void, Void, String> {
+        Context mContext;
+    	DisplayImageOptions options;
+    	protected ImageLoader imageLoader = ImageLoader.getInstance();
+    	
+    	LoadFeaturedContentsTask(Context context) {
+        	super();
+        	this.mContext = context;
+            options = new DisplayImageOptions.Builder()
+    			.showStubImage(R.drawable.loading)
+    			.showImageForEmptyUri(R.drawable.loading)
+    			.cacheInMemory()
+    			.cacheOnDisc()
+    			.build();
+        	imageLoader.init(ImageLoaderConfiguration.createDefault(context));
+
+        }
+    	
+    	protected String doInBackground(Void... V) {
+        	//Featured Item
+            String featuredOutput = HttpUtil.request(Constants.FEATURED_URL);
+            return featuredOutput;
+        }
+
+        protected void onPostExecute(String featuredOutput) {
+        	String featuredString = 
+            		JsonUtil.handleJsonObject(featuredOutput, "products");
+
+            HorizontalScrollView featuredLayout =
+        			(HorizontalScrollView) findViewById(R.id.featured);
+        	LinearLayout featuredHorzScroll = (LinearLayout) 
+        			featuredLayout.findViewById(R.id.horz_scroll_linear);
+    		try {
+            	JSONArray featuredProducts = new JSONArray(featuredString);
+            	for (int i=0; i < featuredProducts.length(); i++) {
+            		String url = JsonUtil.handleJsonObject
+            				(featuredProducts.getString(i), "banner");
+                    String title = 
+                    		JsonUtil.handleJsonObject(
+                    				featuredProducts.getString(i), "name");
+                	String id = 
+                			JsonUtil.handleJsonObject(
+                					featuredProducts.getString(i), "id");
+            		ImageView iv = new ImageView(mContext);
+                	iv.setImageResource(R.drawable.loading);
+                	int minDimen = Util.dpToPx(
+                			getString(R.dimen.featured_horz_height),
+                			context);
+                	iv.setMinimumWidth(minDimen);
+                	iv.setMinimumHeight(minDimen);
+                	
+                	LinearLayout.LayoutParams lp = 
+                			new LinearLayout.LayoutParams(
+                					LinearLayout.LayoutParams.WRAP_CONTENT, 
+                					LinearLayout.LayoutParams.MATCH_PARENT); //w,h
+                	int m = Util.dpToPx(5, context);
+                	lp.setMargins(m, m, m, m);//left,top,right,bottom
+                	iv.setLayoutParams(lp);
+                	//iv.setBackgroundResource(R.layout.background);
+                	
+                	setImageClickListener(iv, getString(R.string.featured_products), title, id);
+                	featuredHorzScroll.addView(iv);
+                	//UrlImageViewHelper.setUrlDrawable(iv, url, 
+                	//		R.drawable.loading);
+                	//iv.setImageResource(R.drawable.starbucks);
+                	imageLoader.displayImage(url ,iv , options);
+                	
+            	}
+                
+            } catch (JSONException e) {
+            	Log.v(TAG, "Json parsing error: " + e);
+            }
+        }
+    }
+    
+    private class LoadCategoryContentsTask extends AsyncTask<Void, Void, String> {
+        Context mContext;
+    	DisplayImageOptions options;
+    	protected ImageLoader imageLoader = ImageLoader.getInstance();
+    	
+    	LoadCategoryContentsTask(Context context) {
+        	super();
+        	this.mContext = context;
+        	imageLoader.init(ImageLoaderConfiguration.createDefault(context));
+            options = new DisplayImageOptions.Builder()
+    			.showStubImage(R.drawable.loading)
+    			.showImageForEmptyUri(R.drawable.loading)
+    			.displayer(new RoundedBitmapDisplayer(20))
+    			.cacheInMemory()
+    			.cacheOnDisc()
+    			.build();
+            imageLoader.init(ImageLoaderConfiguration.createDefault(context));
+
+        }
+    	
+    	protected String doInBackground(Void... V) {
+        	//Featured Item
+    		String aOutput = HttpUtil.request(Constants.HOME_URL);
+            return aOutput;
+        }
+
+        protected void onPostExecute(String aOutput) {
+        	String categoryString = 
+            		JsonUtil.handleJsonObject(aOutput, "categories");
+            
+            HorizontalScrollView categoryLayout =
+        			(HorizontalScrollView) findViewById(R.id.category);
+        	LinearLayout categoryHorzScroll = (LinearLayout) 
+        			categoryLayout.findViewById(R.id.horz_scroll_linear);
+    		try {
+            	JSONArray categoryProducts = new JSONArray(categoryString);
+            	for (int i=1; i < categoryProducts.length(); i++) {
+            		// Starting for i = 1 so we won't show featured item as a category
+            		String url = JsonUtil.handleJsonObject
+            				(categoryProducts.getString(i), "icon");
+            		ImageView iv = new ImageView(context);
+                	iv.setImageResource(R.drawable.loading);
+                	int minDimen = Util.dpToPx(
+                			getString(R.dimen.horz_scroll_height),
+                			context);
+                	iv.setMinimumWidth(minDimen);
+                	iv.setMinimumHeight(minDimen);
+                	
+                	LinearLayout.LayoutParams lp = 
+                			new LinearLayout.LayoutParams(
+                					minDimen, 
+                					LinearLayout.LayoutParams.MATCH_PARENT);//w,h
+                	int m = Util.dpToPx(5, context);
+                	lp.setMargins(m, m, m, m);//left,top,right,bottom
+                	iv.setLayoutParams(lp);
+                	//iv.setBackgroundResource(R.layout.background);
+                	String title = JsonUtil.handleJsonObject(
+                			categoryProducts.getString(i), "name");
+                	String id = JsonUtil.handleJsonObject(
+                			categoryProducts.getString(i), "id");
+                	setImageClickListener(iv, 
+                			getString(R.string.categories), title, id);
+                	categoryHorzScroll.addView(iv);
+                	imageLoader.displayImage(url ,iv , options);
+                	
+            	}
+                
+            } catch (JSONException e) {
+            	Log.v(TAG, "Json parsing error: " + e);
+            }
+    		
+    		// Vendors
+    		String vendorString = 
+            		JsonUtil.handleJsonObject(aOutput, "vendors");
+    		HorizontalScrollView vendorLayout =
+        			(HorizontalScrollView) findViewById(R.id.vendor);
+        	LinearLayout vendorHorzScroll = (LinearLayout) 
+        			vendorLayout.findViewById(R.id.horz_scroll_linear);
+        	
+    		try {
+            	JSONArray vendorProducts = new JSONArray(vendorString);
+            	for (int i=0; i < vendorProducts.length(); i++) {
+            		String url = JsonUtil.handleJsonObject
+            				(vendorProducts.getString(i), "icon");
+            		ImageView iv = new ImageView(context);
+                	iv.setImageResource(R.drawable.loading);
+                	int minDimen = Util.dpToPx(
+                			getString(R.dimen.horz_scroll_height),
+                			context);
+                	iv.setMinimumWidth(minDimen);
+                	iv.setMinimumHeight(minDimen);
+                	
+                	LinearLayout.LayoutParams lp = 
+                			new LinearLayout.LayoutParams(
+                					minDimen, 
+                					LinearLayout.LayoutParams.MATCH_PARENT);//w,h
+                	int m = Util.dpToPx(5, context);
+                	lp.setMargins(m, m, m, m);//left,top,right,bottom
+                	iv.setLayoutParams(lp);
+                	String title = JsonUtil.handleJsonObject(
+                			vendorProducts.getString(i), "name");
+                	String id = JsonUtil.handleJsonObject(
+                			vendorProducts.getString(i), "id");
+                	setImageClickListener(iv, 
+                			getString(R.string.brands), title, id);
+                	//iv.setBackgroundResource(R.layout.background);
+                	vendorHorzScroll.addView(iv);
+                	imageLoader.displayImage(url ,iv , options);
+                	
+            	}
+                
+            } catch (JSONException e) {
+            	Log.v(TAG, "Json parsing error: " + e);
+            }
         }
     }
     
