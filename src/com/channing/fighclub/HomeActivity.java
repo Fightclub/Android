a8231544bbb07d73a4aee72da9306196b64db586
Peter Lee <nadapeter@gmail.com>
2013-02-04 20:50:22 -0800

    Featured item view updated to match iOS

--- src/com/channing/fighclub/HomeActivity.java
+++ src/com/channing/fighclub/HomeActivity.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -18,7 +19,6 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
@@ -143,150 +143,6 @@ public class HomeActivity extends Activity {
     }
     
     
-    private void loadContents() {
-    	
-    	//Featured Item
-        String featruedOutput = HttpUtil.request(Constants.FEATURED_URL);
-        String featuredString = 
-        		JsonUtil.handleJsonObject(featruedOutput, "products");
-
-        HorizontalScrollView featuredLayout =
-    			(HorizontalScrollView) findViewById(R.id.featured);
-    	LinearLayout featuredHorzScroll = (LinearLayout) 
-    			featuredLayout.findViewById(R.id.horz_scroll_linear);
-		try {
-        	JSONArray featuredProducts = new JSONArray(featuredString);
-        	for (int i=0; i < featuredProducts.length(); i++) {
-        		String url = JsonUtil.handleJsonObject
-        				(featuredProducts.getString(i), "icon");
-                String title = 
-                		JsonUtil.handleJsonObject(
-                				featuredProducts.getString(i), "name");
-            	String id = 
-            			JsonUtil.handleJsonObject(
-            					featuredProducts.getString(i), "id");
-        		ImageView iv = new ImageView(this);
-            	iv.setImageResource(R.drawable.loading);
-            	int minDimen = Util.dpToPx(
-            			getString(R.dimen.featured_horz_height),
-            			context);
-            	iv.setMinimumWidth(minDimen);
-            	iv.setMinimumHeight(minDimen);
-            	
-            	LinearLayout.LayoutParams lp = 
-            			new LinearLayout.LayoutParams(
-            					LinearLayout.LayoutParams.WRAP_CONTENT, 
-            					LinearLayout.LayoutParams.MATCH_PARENT); //w,h
-            	int m = Util.dpToPx(5, context);
-            	lp.setMargins(m, m, m, m);//left,top,right,bottom
-            	iv.setLayoutParams(lp);
-            	//iv.setBackgroundResource(R.layout.background);
-            	
-            	setImageClickListener(iv, getString(R.string.featured_products), title, id);
-            	featuredHorzScroll.addView(iv);
-            	UrlImageViewHelper.setUrlDrawable(iv, url, 
-            			R.drawable.loading);
-            	
-        	}
-            
-        } catch (JSONException e) {
-        	Log.v(TAG, "Json parsing error: " + e);
-        }
-		
-		
-		//Categories
-		String aOutput = HttpUtil.request(Constants.HOME_URL);
-        String categoryString = 
-        		JsonUtil.handleJsonObject(aOutput, "categories");
-        
-        HorizontalScrollView categoryLayout =
-    			(HorizontalScrollView) findViewById(R.id.category);
-    	LinearLayout categoryHorzScroll = (LinearLayout) 
-    			categoryLayout.findViewById(R.id.horz_scroll_linear);
-		try {
-        	JSONArray categoryProducts = new JSONArray(categoryString);
-        	for (int i=0; i < categoryProducts.length(); i++) {
-        		String url = JsonUtil.handleJsonObject
-        				(categoryProducts.getString(i), "icon");
-        		ImageView iv = new ImageView(this);
-            	iv.setImageResource(R.drawable.loading);
-            	int minDimen = Util.dpToPx(
-            			getString(R.dimen.horz_scroll_height),
-            			context);
-            	iv.setMinimumWidth(minDimen);
-            	iv.setMinimumHeight(minDimen);
-            	
-            	LinearLayout.LayoutParams lp = 
-            			new LinearLayout.LayoutParams(
-            					minDimen, 
-            					LinearLayout.LayoutParams.MATCH_PARENT);//w,h
-            	int m = Util.dpToPx(5, context);
-            	lp.setMargins(m, m, m, m);//left,top,right,bottom
-            	iv.setLayoutParams(lp);
-            	//iv.setBackgroundResource(R.layout.background);
-            	String title = JsonUtil.handleJsonObject(
-            			categoryProducts.getString(i), "name");
-            	String id = JsonUtil.handleJsonObject(
-            			categoryProducts.getString(i), "id");
-            	setImageClickListener(iv, 
-            			getString(R.string.categories), title, id);
-            	categoryHorzScroll.addView(iv);
-            	UrlImageViewHelper.setUrlDrawable(iv, url, 
-            			R.drawable.loading);
-            	
-        	}
-            
-        } catch (JSONException e) {
-        	Log.v(TAG, "Json parsing error: " + e);
-        }
-		
-		
-		// Vendors
-		String vendorString = 
-        		JsonUtil.handleJsonObject(aOutput, "vendors");
-		HorizontalScrollView vendorLayout =
-    			(HorizontalScrollView) findViewById(R.id.vendor);
-    	LinearLayout vendorHorzScroll = (LinearLayout) 
-    			vendorLayout.findViewById(R.id.horz_scroll_linear);
-    	
-		try {
-        	JSONArray vendorProducts = new JSONArray(vendorString);
-        	for (int i=0; i < vendorProducts.length(); i++) {
-        		String url = JsonUtil.handleJsonObject
-        				(vendorProducts.getString(i), "icon");
-        		ImageView iv = new ImageView(this);
-            	iv.setImageResource(R.drawable.loading);
-            	int minDimen = Util.dpToPx(
-            			getString(R.dimen.horz_scroll_height),
-            			context);
-            	iv.setMinimumWidth(minDimen);
-            	iv.setMinimumHeight(minDimen);
-            	
-            	LinearLayout.LayoutParams lp = 
-            			new LinearLayout.LayoutParams(
-            					minDimen, 
-            					LinearLayout.LayoutParams.MATCH_PARENT);//w,h
-            	int m = Util.dpToPx(5, context);
-            	lp.setMargins(m, m, m, m);//left,top,right,bottom
-            	iv.setLayoutParams(lp);
-            	String title = JsonUtil.handleJsonObject(
-            			vendorProducts.getString(i), "name");
-            	String id = JsonUtil.handleJsonObject(
-            			vendorProducts.getString(i), "id");
-            	setImageClickListener(iv, 
-            			getString(R.string.brands), title, id);
-            	//iv.setBackgroundResource(R.layout.background);
-            	vendorHorzScroll.addView(iv);
-            	UrlImageViewHelper.setUrlDrawable(iv, url, 
-            			R.drawable.loading);
-            	
-        	}
-            
-        } catch (JSONException e) {
-        	Log.v(TAG, "Json parsing error: " + e);
-        }
-    }
-    
     
     private class LoadFeaturedContentsTask extends AsyncTask<Void, Void, String> {
         Context mContext;
@@ -339,13 +195,18 @@ public class HomeActivity extends Activity {
                 	iv.setMinimumWidth(minDimen);
                 	iv.setMinimumHeight(minDimen);
                 	
+                	DisplayMetrics dm = new DisplayMetrics();
+                	getWindowManager().getDefaultDisplay().getMetrics(dm);
+                	int width = dm.widthPixels;
+                	
                 	LinearLayout.LayoutParams lp = 
                 			new LinearLayout.LayoutParams(
-                					LinearLayout.LayoutParams.WRAP_CONTENT, 
+                					width, 
                 					LinearLayout.LayoutParams.MATCH_PARENT); //w,h
-                	int m = Util.dpToPx(5, context);
-                	lp.setMargins(m, m, m, m);//left,top,right,bottom
+                	//int m = Util.dpToPx(5, context);
+                	lp.setMargins(0, 0, 0, 0);//left,top,right,bottom
                 	iv.setLayoutParams(lp);
+                	iv.setScaleType(ImageView.ScaleType.FIT_XY);
                 	//iv.setBackgroundResource(R.layout.background);
                 	
                 	setImageClickListener(iv, getString(R.string.featured_products), title, id);
